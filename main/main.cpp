8bbdde5af9bdce941c2578eab30fe9c398d5ee05
Nannk <nannk@cock.li>
2024-06-13 09:33:46 +0200

    nothing really changed, still not working

--- main/main.cpp
+++ main/main.cpp
@@ -22,7 +22,6 @@ static Mpu mpu;
 uint8_t sensor_data[14];
 uint8_t who_am_i, i;
 double Temp;
-static uint32_t error_count = 0;
 int ret;
 long long current_millis;
 long long previous_millis;
@@ -39,7 +38,7 @@ void mpu_task(void *Parameters) {
   ESP_LOGI(TAG, "done setting up");
   // ears.choose_pose(21);
   // ears.move_to_set_angles();
-  // vTaskDelay(100 / portTICK_RATE_MS); // for assembling wing correctly
+  // vTaskDelay(1000 / portTICK_RATE_MS); // for assembling wing correctly
 
   // test thing
   ears.choose_pose(31);
@@ -51,13 +50,20 @@ void mpu_task(void *Parameters) {
     // mpu.mpu9250_read(I2C_MASTER_NUM, WHO_AM_I, &who_am_i, 1);
 
     // memset(sensor_data, 0, 14);
-    ret = mpu.mpu9250_read(I2C_MASTER_NUM, ACCEL_XOUT_H, sensor_data, 1);
+    ret = mpu.mpu9250_read(I2C_MASTER_NUM, ACCEL_XOUT_H, sensor_data, 14);
 
     if (ret == ESP_OK) {
       // ESP_LOGI(TAG, "*******************");
 
       // for (i = 0; i < 14; i++) {
-      ESP_LOGI(TAG, "%d ", (int16_t)(sensor_data[0]));
+      ESP_LOGI(TAG, "%d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d",
+               (int16_t)(sensor_data[0]), (int16_t)(sensor_data[1]),
+               (int16_t)(sensor_data[2]), (int16_t)(sensor_data[3]),
+               (int16_t)(sensor_data[4]), (int16_t)(sensor_data[5]),
+               (int16_t)(sensor_data[6]), (int16_t)(sensor_data[7]),
+               (int16_t)(sensor_data[8]), (int16_t)(sensor_data[9]),
+               (int16_t)(sensor_data[10]), (int16_t)(sensor_data[11]),
+               (int16_t)(sensor_data[12]), (int16_t)(sensor_data[13]));
       //}
 
       // ESP_LOGI(TAG, "error_count: %d\n", error_count);
@@ -68,15 +74,12 @@ void mpu_task(void *Parameters) {
     // ESP_LOGD(TAG, "memory: %d",
     // (int)heap_caps_get_free_size(MALLOC_CAP_32BIT));
 
-    current_millis = esp_timer_get_time() / 1000;
     vTaskDelay(10 / portTICK_PERIOD_MS);
-    previous_millis = current_millis;
   }
-  // xTaskCreate(pose_looping, "loop though poses", 4096, NULL, 1, NULL);
   i2c_driver_delete(I2C_MASTER_NUM);
   fflush(stdout);
 }
 
 extern "C" void app_main() {
-  xTaskCreate(mpu_task, "mpu_tasl", 2048, NULL, 10, NULL);
+  xTaskCreate(mpu_task, "mpu_task", 2048, NULL, 10, NULL);
 }
